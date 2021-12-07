a8c85941c9fab275cb68825f43ab2db96e1ee025
Sebastian Sole <scsole@protonmail.com>
2021-12-07 15:39:32 +1300

    Initial docker setup with database

--- src/index.php
+++ src/index.php
@@ -2,4 +2,5 @@
 <?php require('components/head.inc.php'); ?>
 <?php include('components/navbar.inc.php'); ?>
 <?php include('components/header.inc.php'); ?>
+<?php include('components/test-db.inc.php'); ?>
 <?php require('components/footer.inc.php'); ?>
